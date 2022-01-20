16c1f39ef9d98468926b1b69af822a4a11c65a82
Charles Ford <charles.ford1978@gmail.com>
2022-01-20 15:36:21 -0500

    Add project files.

---
+++ SeleniumTest/Sample1.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTest
+{
+    internal class Sample1
+    {
+        //create the reference for the browser
+        IWebDriver driver = new ChromeDriver();
+
+        public void Initialize()
+        {
+            //navigate to URL
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+
+            //maximize the browser window
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+        }
+
+        public void ExecuteTest()
+        {
+
+        }
+
+        public void EndTest()
+        {
+
+        }
+
+    }
+}
