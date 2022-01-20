52876229d4fa03e967d466747e3e3f2164602290
Charles Ford <charles.ford1978@gmail.com>
2022-01-20 16:32:25 -0500

    Added code to the ExecuteTest and EndTest methods.  Added nUnit attributes to all 3 methods.

--- SeleniumTest/Sample1.cs
+++ SeleniumTest/Sample1.cs
@@ -1,18 +1,21 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SeleniumTest
 {
-    internal class Sample1
+    class Sample1
     {
         //create the reference for the browser
         IWebDriver driver = new ChromeDriver();
 
+        [SetUp]
         public void Initialize()
         {
             //navigate to URL
@@ -23,14 +26,36 @@ namespace SeleniumTest
             Thread.Sleep(2000);
         }
 
+        [Test]
         public void ExecuteTest()
         {
+            //identify the username text box
+            IWebElement ele = driver.FindElement(By.Id("email"));
 
+            //enter the username value
+            ele.SendKeys("charles.ford1978@gmail.com");
+            Thread.Sleep(2000);
+            Console.Write("username value is entered \n");
+
+            //identify the password text box
+            IWebElement ele1 = driver.FindElement(By.Name("pass"));
+
+            //enter the password value
+            ele1.SendKeys("ratsoup78");
+            Console.Write("password is entered \n");
+
+            //click on the login button
+            IWebElement ele2 = driver.FindElement(By.Id("u_0_d_Pt"));
+            ele2.Click();
+            Thread.Sleep(3000);
+            Console.Write("login button is clicked \n");
         }
 
+        [TearDown]
         public void EndTest()
         {
-
+            //close the browser
+            driver.Close();
         }
 
     }
